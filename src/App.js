13578c7e50f522723f035ad52d824c044b39d7b2
Vittorio Camisa <vittorio@igenius.net>
2018-07-07 11:03:46 +0200

    Init react tutorial plus refactor

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import * as R from 'ramda'
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
+const Square = R.curry((props, i) => (
+	<button className="square" onClick={() => props.onClick(i)}>
+		{props.squares[i]}
+	</button>
+))
+
+const BoardRow = (props) => {
+	const squares = R.map(Square(props), props.indexes)
+	return <div className="board-row"> {squares} </div>
+}
+
+const Board = (props) => (
+	<div>
+		<BoardRow squares={props.squares} onClick={props.onClick} indexes={R.range(0, 3)} />
+		<BoardRow squares={props.squares} onClick={props.onClick} indexes={R.range(3, 6)} />
+		<BoardRow squares={props.squares} onClick={props.onClick} indexes={R.range(6, 9)} />
+	</div>
+)
+
+
+class Game extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			history: [{squares: Array(9).fill(null)}],
+			stepNumber: 0,
+			xIsNext: true
+		}
+	}
+
+	handleClick(i) {
+		const history = this.state.history.slice(0, this.state.stepNumber + 1)
+		const current = history[history.length - 1]
+		const squares = current.squares.slice()
+		if (calculateWinner(squares) || squares[i]) {
+			return
+		}
+		squares[i] = this.state.xIsNext ? 'X' : 'O'
+		this.setState({
+			history: history.concat([{
+				squares: squares,
+			}]),
+			stepNumber: history.length,
+			xIsNext: !this.state.xIsNext,
+		})
+	}
+
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		})
+	}
+
+	render() {
+		const history = this.state.history
+		const current = history[this.state.stepNumber]
+		const winner = calculateWinner(current.squares)
+
+		const moves = history.map((step, move) => {
+			const desc = move ? `Go to move # ${move}` : 'Go to start'
+			return (
+				<li key={move}>
+					<button onClick={() => this.jumpTo(move)}>{desc}</button>
+				</li>
+			)
+		})
+
+		let status
+		if (winner) {
+			status = 'Winner: ' + winner
+		} else {
+			status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+		}
+		return (
+			<div className="game">
+				<div className="game-board">
+					<Board
+						squares={current.squares}
+						onClick={(i) => this.handleClick(i)}
+					/>
+				</div>
+				<div className="game-info">
+					<div>{status}</div>
+					<ol>{moves}</ol>
+				</div>
+			</div>
+		)
+	}
+}
+
+
+
+const App = () => <Game />
+
+export default App
