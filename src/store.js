2f1fc130a12d8b9d4613f3d518d8f638c5052298
Provok <vittorio.camisa@gmail.com>
2018-07-21 16:26:10 +0200

    Added redux devtools basic configuration

--- src/store.js
+++ src/store.js
@@ -1,6 +1,16 @@
 import { createStore } from "redux"
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import rootReducer from "./reducers"
 
-const store = createStore(rootReducer)
+function Store(debug) {
+	if (debug) {
+		return createStore(rootReducer, devToolsEnhancer())
+	} else {
+		return createStore(rootReducer)
+	}
+}
+
+const store = Store(true)
 
 export default store
